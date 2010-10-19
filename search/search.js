1259f15544121c05f4be1b1dbcdc7fe1a5024eeb
Hay <huskyr@gmail.com>
2010-10-20 01:09:23 +0200

    adding in the new model again

--- search/search.js
+++ search/search.js
@@ -1,14 +1,71 @@
+window.log = (console && console.log) ? console.log : function(){alert();};
+
 $(document).ready(function() {
-    $("#search").keyup(function() {
-        var search = $("#search").val();
-        var url = 'http://commons.wikimedia.org/w/api.php?action=opensearch&format=json&callback=?&search=' + search;
-        $.getJSON(url, function(data) {
-            $("#results").empty();
-            $.each(data[1], function() {
-                $("#results").append('<li><a>' + this + '</a></li>');
+    function getQueryUrl(type, args) {
+        var queries = {
+            "pagesearch" : function(q) {
+                return {
+                    "action" : "opensearch",
+                    "search" : q.search
+                };
+            },
+
+            "pageimages" : function(q) {
+                return {
+                    "action" : "query",
+                    "indexpageids" : 1,
+                    "titles" : q.title
+                };
+            },
+
+            "thumbs" : function(q) {
+                return {
+                    "action" : "query",
+                    "prop" : "imageinfo",
+                    "iiprop" : "url",
+                    "iiurlwidth" : q.width,
+                    "indexpageids" : 1,
+                    "titles" : q.image
+                };
+            }
+        }
+
+        if (!queries[type]) return false;
+
+        return makeUrl(queries[type](args));
+    }
+
+    function makeUrl(args) {
+        // default arguments
+        args = $.extend({
+            "format" : "json",
+            "callback" : "!noencode!?"
+        }, args);
+
+        var url = 'http://commons.wikimedia.org/w/api.php';
+        var first = true;
+        for (var key in args) {
+            var value = args[key];
+            url += (first) ? "?" : "&";
+            first = false;
+            value = (value.indexOf("!noencode!") === 0) ? value.slice(10) : encodeURIComponent(value);
+            url += key + '=' + value;
+        }
+        return url;
+    }
+
+    $("#search").autocomplete({
+        source : function(request, response) {
+            var url = getQueryUrl("pagesearch", {
+                "search" : $("#search").val()
             });
-        });
+
+            $.getJSON(url, function(data) {
+                response(data[1]);
+            });
+        }
     });
+
 
     $("#results a").live('click', function() {
         $("#images").empty();
